923a2d7d179b8be6a5d58d04fc28321bb206ec4f
isaiasilvadh <isaiasilva64@gmail.com>
2022-07-04 19:42:22 -0500

    clase

--- ejemplos/ejemplo1/proyectoUno/administrativo/views.py
+++ ejemplos/ejemplo1/proyectoUno/administrativo/views.py
@@ -133,7 +133,8 @@ def crear_numero_telefonico(request):
 
     return render(request, 'crearNumeroTelefonico.html', diccionario)
 
-
+@login_required(login_url='/entrando/login/')
+@permission_required('administrativo.change_numerotelefonico', login_url="/entrando/login/")
 def editar_numero_telefonico(request, id):
     """
     """
@@ -150,6 +151,8 @@ def editar_numero_telefonico(request, id):
 
     return render(request, 'crearNumeroTelefonico.html', diccionario)
 
+@login_required(login_url='/entrando/login/')
+@permission_required('administrativo.can_create_numero_telefonico_sp2', login_url="/entrando/login/")
 def crear_numero_telefonico_estudiante(request, id):
     """
     """
